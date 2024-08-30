32c167c25f6b464d05dbc16f5e04250973f2bcd2
Peter Schmit <schmitpete@gmail.com>
2024-08-29 21:36:37 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Peter Schmit
+ *         pschmit3@huskers.unl.edu
+ * Date: 2024/08/29
+ *
+ * Yet another simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Peter Schmit\n");
+
+    return 0;
+
+}
